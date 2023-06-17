f3a4651feba1768879b3e4cda1c6f7afea7cdca6
Tiago de Castro Lima <casmei@protonmail.com>
2023-06-17 16:49:54 -0300

    ✨add new return component

---
+++ resources/views/components/return-button.blade.php
@@ -0,0 +1,6 @@
+<button {{ $attributes->merge(['type' => 'button', 'class' => ' inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    <svg class='mr-2' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+    </svg>
+    VOLTAR
+</button>
