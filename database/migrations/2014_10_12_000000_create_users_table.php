f0a1b2bf35d2e28003fe3ee9e5ff61a8a6a16b32
Tiago de Castro Lima <casmei@protonmail.com>
2023-06-17 23:50:32 -0300

    ✨ create positions table migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role_id')->default(1);
