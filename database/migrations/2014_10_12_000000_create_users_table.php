07ab32c13dc0cb297ed69cd930f9d934dfbad2aa
Tiago de Castro Lima <casmei@protonmail.com>
2023-06-19 10:38:25 -0300

    ✨ add into User password_changed column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('cpf')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role_id')->default(1);
             $table->rememberToken();
