5dd183119070e2d4cff0d2dffebf0889d607fc20
Tiago de Castro Lima <casmei@protonmail.com>
2023-06-19 10:40:12 -0300

    ✨ Implement modal into dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,63 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @can('senha-atualizada')
+                <x-changed-password name="change-password" :show="true" >
+                    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        <div class="max-w-xl">
+                            <section>
+                                <header>
+                                    <h2 class="text-lg font-medium text-gray-900 mb-4">
+
+                                                <p style="color:#b0233a;">Por motivos de segurança, é necessário que você atualize sua senha imediatamente!</p>
+                                    </h2>
+
+                                    <p class="mt-1 text-sm text-gray-600">
+                                        Por favor, escolha uma senha forte e exclusiva para proteger sua conta. Se tiver alguma dúvida, entre em contato com o suporte técnico.
+                                    </p>
+                                </header>
+
+                                <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+                                    @csrf
+                                    @method('put')
+
+                                    <div>
+                                        <x-input-label for="current_password" :value="__('Senha atual')" />
+                                        <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+                                        <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+                                    </div>
+
+                                    <div>
+                                        <x-input-label for="password" :value="__('Nova senha')" />
+                                        <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                                        <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+                                    </div>
+
+                                    <div>
+                                        <x-input-label for="password_confirmation" :value="__('Confirme a senha')" />
+                                        <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                                        <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+                                    </div>
+
+                                    <div class="flex items-center gap-4">
+                                        <x-primary-button>{{ __('Atualizar') }}</x-primary-button>
+
+                                        @if (session('status') === 'password-updated')
+                                            <p
+                                                x-data="{ show: true }"
+                                                x-show="show"
+                                                x-transition
+                                                x-init="setTimeout(() => show = false, 2000)"
+                                                class="text-sm text-gray-600"
+                                            >{{ __('Saved.') }}</p>
+                                        @endif
+                                    </div>
+                                </form>
+                            </section>
+                        </div>
+                    </div>
+                </x-changed-password>
+            @endcan
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
