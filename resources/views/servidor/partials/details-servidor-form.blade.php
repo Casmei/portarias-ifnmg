3554815b94da2304b87208cf8ddee335eeca30da
Matheus <matheuscontato96@hotmail.com>
2024-02-29 17:00:27 -0300

    REFACTOR: alterando o layout da pagina de detalhes do servidor

--- resources/views/servidor/partials/details-servidor-form.blade.php
+++ resources/views/servidor/partials/details-servidor-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header class="mb-4">
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-xl font-medium text-gray-900">
             Detalhes do servidor {{ $servidor?->name }}
         </h2>
     </header>
 
-    <div class="space-y-5 mt-10">
+    <div class="mt-10 grid grid-cols-2  gap-x-60 gap-y-8">
         <div>
             <x-input-label for="servidor-name" :value="__('Nome do Servidor')" />
             <p>{{ $servidor?->name }}</p>
@@ -26,19 +26,19 @@
 
         <div>
             <x-input-label for="cargo" :value="__('Cargo')" />
-            {{ $position->name ?? '' }}
+            {{ $position->name ?? 'não informado' }}
             <x-input-error :messages="$errors->get('position_id')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="funcao" :value="__('Função')" />
-            {{ $funcao->name ?? '' }}
+            {{ $funcao->name ?? 'não informado' }}
             <x-input-error :messages="$errors->get('funcao_id')" class="mt-2" />
         </div>
 
         <div>
             <x-input-label for="cargo" :value="__('SIAPE')" />
-            {{ $servidor->siape }}
+            {{ $servidor->siape ?? 'não informado' }}
             <x-input-error :messages="$errors->get('position_id')" class="mt-2" />
         </div>
     </div>
