c75804a600611ab86b0c88fb5a27b1685f9b83b1
Isac Ferreira Celestino <102570294+isacfc45@users.noreply.github.com>
2024-02-29 18:53:51 -0300

    Update resources/views/servidor/details.blade.php
    Co-authored-by: Tiago de Castro Lima <68354933+Casmei@users.noreply.github.com>

--- resources/views/servidor/details.blade.php
+++ resources/views/servidor/details.blade.php
@@ -34,7 +34,7 @@
             </div>
         </div>
     </div>
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="flex flex-col mx-5">
